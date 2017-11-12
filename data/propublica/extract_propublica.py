61e8063be90a6af125361f58f3c4be5b35ec195c
winlingit <winlin@gmail.com>
2017-11-11 20:13:02 -0600

    finished extract functions for recent member votes

--- data/propublica/extract_propublica.py
+++ data/propublica/extract_propublica.py
@@ -7,8 +7,7 @@ import logging
 
 headers = {'X-API-Key': '5dHwl0cO6ak64MN9Q8IwZDkGHg4bGazYhBD83vBs'}
 
-logging.basicConfig(filename='pp_errors.log',
-                    level=logging.DEBUG)
+logging.basicConfig(filename='pp_errors.log', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class PropublicaApiError(Exception):
@@ -35,6 +34,8 @@ def api_call(params):
         pass
 
 
+######## MEMBERS ########
+
 # get list of members for given Congress
 def getMembers(chamber, session, to_csv=False):
     params = [str(session).encode('utf-8'), chamber, 'members.json']
@@ -43,6 +44,7 @@ def getMembers(chamber, session, to_csv=False):
         member_ids = writeMembersCSV(json_file=response, status='current', chamber=chamber, session=session)
         return member_ids
     return response
+
 
 # get list of new members for given Congress
 def getNewMembers(to_csv=False):
@@ -53,6 +55,7 @@ def getNewMembers(to_csv=False):
         return member_ids
     return response
 
+
 # get list of members leaving after given Congress
 def getLeavingMembers(chamber, session, to_csv=False):    
     params = [str(session).encode('utf-8'), chamber, 'members/leaving.json']
@@ -62,29 +65,16 @@ def getLeavingMembers(chamber, session, to_csv=False):
         return member_ids
     return response
 
-# get vote history for given member
-def getMemberVotes(member_id, to_csv=False):
-    params = ['members', member_id, 'votes.json']
-    response = api_call(params)
-    if to_csv and response is not None:
-        writeMemberVotesCSV(json_file=response, member_id=member_id)
-    return response
-
-# get bills cosponsored by given member
-def getMemberBills(member_id, to_csv=False):
-    params = ['members', member_id, 'bills/cosponsored.json']
-    response = api_call(params)
-    return response
-
 
 # write list of members to CSV
 def writeMembersCSV(json_file, status='current', chamber='senate', session=115):
 
     # load member records from JSON string to dataframe
     results = json.loads(json.dumps(json_file))['results'][0]
-    members = json.dumps(results['members'])
-    df = pd.read_json(members, orient='records')
+    members = results['members']
+    df = pd.DataFrame.from_dict(members)
     df = df.assign(session = pd.Series([session]*df.shape[0], index=df.index))
+    
     # set file names based on member status current, leaving, or new
     if status is 'current':
         file = 'raw/members_%s_%i.csv' % (chamber, session)
@@ -95,15 +85,81 @@ def writeMembersCSV(json_file, status='current', chamber='senate', session=115):
 
     # write dataframe to CSV
     df.to_csv(file, encoding='utf-8', index=False)
-    return df.id
     
-# write list of votes by member to CSV
-def writeMemberVotesCSV(json_file, member_id):
+
+# get Senate members for Congresses 80-115 or House members for Congresses 102-115
+def getAllMembers(chamber='senate'):
+    start = 80 if chamber == 'senate' else 102
+    
+    for i in range(start,116):
+        member_ids = getMembers(chamber, i, to_csv=True)
+        getLeavingMembers(chamber, i, to_csv=True)
+
+
+# combine Senate members into single file
+def mergeMembers(chamber='senate'):
+    start = 80 if chamber == 'senate' else 102
+
+    merged = pd.concat([pd.read_csv('raw/members_%s_%i.csv' % (chamber, i)) for i in range(start,116)], ignore_index=True)
+    merged.to_csv('members_%s.csv' % (chamber), index=False)
+
+
+
+
+######## VOTES ########
+
+# get recent votes for given member
+def getRecentVotes(member_id, to_csv=False):
+    params = ['members', member_id, 'votes.json']
+    response = api_call(params)
+    if to_csv and response is not None:
+        writeRecentVotesCSV(json_file=response, member_id=member_id)
+    return response
+
+
+# write list of recent votes by member to CSV
+def writeRecentVotesCSV(json_file, member_id):
     results = json.loads(json.dumps(json_file))['results'][0]
-    votes = json.dumps(results['votes'])
+    votes = results['votes']
+    
+    # if votes exist for member, write to CSV
     if len(votes) > 2:
-        df = pd.read_json(votes, orient='records')
+        df = pd.DataFrame.from_dict(votes)
+        
+        # add columns for bill ID, sponsor ID, bill URI if available
+        df['bill_id'] = df['bill'].apply(lambda x: x['bill_id'].split('-')[0] if len(x) > 0 and 'bill_id' in x.keys() else 'NA')
+        df['sponsor_id'] = df['bill'].apply(lambda x: x['sponsor_id'] if len(x) > 0 and 'sponsor_id' in x.keys() else 'NA')
+        df['bill_uri'] = df['bill'].apply(lambda x: x['bill_uri'] if len(x) > 0 and 'bill_uri' in x.keys() else 'NA')
         df.to_csv('raw/member_votes_%s.csv' % (member_id), encoding='utf-8', index=False)
+
+        
+# combine recent member votes into single file
+def getAllRecentVotes(chamber='senate'):
+    # get member IDs from master members file
+    members = pd.read_csv('members_%s.csv' % (chamber))
+    member_ids = members.id.unique()
+
+    for i in member_ids:
+        # if vote file for member does not exist, get member votes
+        if not os.path.isfile('raw/member_votes_%s.csv' % (i)):
+            getRecentVotes(i, to_csv=True)
+            
+            
+# combine votes into single file
+def mergeRecentVotes():
+    merged = pd.concat([pd.read_csv('raw/%s' % (filename)) for filename in os.listdir('raw') if filename.startswith('member_votes_')], ignore_index=True)
+    merged.to_csv('member_votes.csv', index=False)
+    
+
+
+######## BILLS ########
+
+# get bills cosponsored by given member
+def getMemberBills(member_id, to_csv=False):
+    params = ['members', member_id, 'bills/cosponsored.json']
+    response = api_call(params)
+    return response
+
 
 # write list of bills cosponsored by member to CSV
 def writeMemberBillsCSV(json_file, member_id):
@@ -113,38 +169,14 @@ def writeMemberBillsCSV(json_file, member_id):
     df.to_csv('member_bills_%s.csv' % (member_id), encoding='utf-8', index=False)
 
 
+def getBill(bill_id, to_csv=False):
+    pass
 
-# get Senate members for Congresses 80-115 or House members for Congresses 102-115
-def getAllMembers(chamber='senate'):
-    start = 80 if chamber == 'senate' else 102
+
     
-    for i in range(start,116):
-        member_ids = getMembers(chamber, i, to_csv=True)
-        getLeavingMembers(chamber, i, to_csv=True)
-    return member_ids
 
-# combine Senate members into single file
-def mergeMembers(chamber='senate'):
-    start = 80 if chamber == 'senate' else 102
 
-    merged = pd.concat([pd.read_csv('members_%s_%i.csv' % (chamber, i)) for i in range(start,116)], ignore_index=True)
-    merged.to_csv('members_%s.csv' % (chamber))
 
-# combine members into single file
-def getAllVotes(chamber='senate'):
-    # get member IDs from master members file
-    members = pd.read_csv('members_%s.csv' % (chamber))
-    member_ids = members.id.unique()
-
-    for i in member_ids:
-        # if vote file for member does not exist, get member votes
-        if not os.path.isfile('member_votes_%s.csv' % (i)):
-            getMemberVotes(i, to_csv=True)
-
-# combine votes into single file
-def mergeVotes():
-    merged = pd.concat([pd.read_csv('raw/%s' % (filename)) for filename in os.listdir('raw') if filename.startswith('member_votes_')], ignore_index=True)
-    merged.to_csv('member_votes.csv')
 
 if __name__ == '__main__':
 
@@ -154,8 +186,8 @@ if __name__ == '__main__':
     # getAllMembers('house')
     # mergeMembers('house')
 
-    # getAllVotes('senate')
-    # getAllVotes('house')
+    # getAllRecentVotes('senate')
+    # getAllRecentVotes('house')
     # mergeVotes()
 
 

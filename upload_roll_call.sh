cb7366b3941e76c9cd87ec399d7728c27b7c85fd
winlingit <winlin@gmail.com>
2017-11-14 08:59:04 -0600

    added functions to pull all votes by month/year and all associated roll call votes; added scripts to scp to VM

---
+++ upload_roll_call.sh
@@ -0,0 +1,48 @@
+scp ./data/propublica/raw/votes_roll_call_senate_103* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_102* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_101* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_100* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_99* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_98* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_97* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_96* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_95* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_94* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_93* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_92* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_91* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_90* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_89* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_88* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_87* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_86* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_85* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_84* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_83* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_82* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_81* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+
+scp ./data/propublica/raw/votes_roll_call_senate_80* root@50.97.218.77:/root/mids-w210-capstone/data/propublica/raw
+

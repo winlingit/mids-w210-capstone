3f0b92e3d758c9e475b00519bca18a0fb000e410
Matthew Burke <mwburke@berkeley.edu>
2017-12-04 20:33:22 -0800

    track predictions working + images for contribute

--- flipflop/blueprints/track/views.py
+++ flipflop/blueprints/track/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request
 from flipflop.blueprints.track.models import Bill, BillVote, BillPrediction, Model
 from flipflop.blueprints.find.models import Member
 from flipflop.extensions import db
-from sqlalchemy import func
+from sqlalchemy import func, desc
 import re
 
 track = Blueprint('track', __name__, template_folder='templates', url_prefix='/track')
@@ -33,13 +33,16 @@ def track_page(bill_id=None):
 		else:
 			cosponsors = {'-'}
 		
-		top_predictions = BillVote.query.filter(BillVote.bill_id==bill_id).join(BillPrediction).join(Member).join(Model).order(BillPrediction.pred_probs).limit(20).all()
+		top_predictions = db.session.query(BillVote, BillPrediction, Member, Model).filter(BillVote.bill_id==bill_id).join(BillPrediction).join(Member).join(Model).order_by(desc(BillPrediction.pred_probs)).limit(20).all()
 
 		print(bill_info[0])
 		print(sponsor[0])
 		print(cosponsors)
 		print(len(top_predictions))
 		print(top_predictions[0])
+		print(top_predictions[0].Member.first_name)
+		print(top_predictions[0].BillPrediction.pred_probs)
+		print(top_predictions[0].Model.model)
 
 		return render_template('track/track_detail.html', 
 								bill_info=bill_info[0],
